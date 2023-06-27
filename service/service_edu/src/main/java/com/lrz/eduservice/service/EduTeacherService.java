1c951c50cacfc72559fbc05cc568c58d1df1f6ee
LRZ <1015450627@qq.com>
2023-06-27 11:56:35 +0800

    6.27 讲师管理模块

---
+++ service/service_edu/src/main/java/com/lrz/eduservice/service/EduTeacherService.java
@@ -0,0 +1,16 @@
+package com.lrz.eduservice.service;
+
+import com.lrz.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2023-06-27
+ */
+public interface EduTeacherService extends IService<EduTeacher> {
+
+}
---
+++ service/service_edu/src/main/java/com/lrz/eduservice/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.lrz.eduservice.service.impl;
+
+import com.lrz.eduservice.entity.EduTeacher;
+import com.lrz.eduservice.mapper.EduTeacherMapper;
+import com.lrz.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2023-06-27
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+}
